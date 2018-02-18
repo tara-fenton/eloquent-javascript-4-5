2db096424e3864dd1dca3d1e4f7e11c039ba1e88
Tara Fenton <tarafenton@Taras-MBP.fios-router.home>
2018-02-17 21:28:26 -0500

    sum function added

--- sum-of-a-range.js
+++ sum-of-a-range.js
@@ -12,6 +12,14 @@ function range(start, end){
 //Next, write a sum function that takes an array of numbers and returns the sum of these numbers.
 //Run the previous program and see whether it does indeed return 55.
 
+function sum(arr) {
+  var total = 0;
+  for (var i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
+}
+
 //As a bonus assignment, modify your range function to take an optional third argument
 //that indicates the “step” value used to build up the array. If no step is given,
 //the array elements go up by increments of one, corresponding to the old behavior.
@@ -22,5 +30,5 @@ console.log(range(1, 10));
 // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 //console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
-//console.log(sum(range(1, 10)));
+console.log(sum(range(1, 10)));
 // → 55
